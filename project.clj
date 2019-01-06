98cbc469c8fcc08f936e3aecdc18eaf65051c0b4
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-06 21:21:35 +0530

    Update Clojure to 1.10.0

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/divs1210/conquerant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :plugins [[lein-cloverage "1.0.13"]])
