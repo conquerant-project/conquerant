41543e67eb30c308689b410e27a7b2df64af45df
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-07 01:41:18 +0530

    Release 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conquerant "0.4.2"
+(defproject conquerant "1.0.0"
   :description "async/await for Clojure"
   :url "https://github.com/divs1210/conquerant"
   :license {:name "Eclipse Public License"
