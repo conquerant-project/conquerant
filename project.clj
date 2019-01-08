690b56f07835bfaa1bd6a1e787d6b9d1ec62144f
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-09 02:33:50 +0530

    Put cloverage dependency in dev profile

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :plugins [[lein-cloverage "1.0.13"]])
+  :profiles {:dev {:plugins [[lein-cloverage "1.0.13"]]}})
