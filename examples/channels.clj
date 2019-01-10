81e7153c65c7951dbc7bf736b28e15b1e730f181
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-10 23:24:18 +0530

    refactor

--- examples/channels.clj
+++ examples/channels.clj
@@ -26,10 +26,12 @@
        (take! ch)))))
 
 
-(defn run-test []
-  (let [c (chan)]
-    (dotimes [i 100]
-      (c/async (let [x (c/await (take! c))]
-                 (println x))))
-    (dotimes [i 100]
-      (put! c :hi))))
+(comment
+  (def c (chan))
+
+  (dotimes [i 100]
+    (c/async (let [x (c/await (take! c))]
+               (println x))))
+
+  (dotimes [i 100]
+    (put! c :hi)))
